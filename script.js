7280838631a8056418a92b18a56570f97d8223bd
cdyoung1 <41019337+cdyoung1@users.noreply.github.com>
2018-07-24 15:06:05 -0700

    Grade Table Commit 1

---
+++ script.js
@@ -0,0 +1,65 @@
+let student = [];
+let gradeArray = [];
+let studentBody;
+const passGrade=70;
+let avggrade=0;
+
+function createNewRow(data){
+  const passColor = (parseInt(data.grade)>=70) ? "green" : "red";
+  
+  return `<tr>
+  <td><input type="checkbox"class="rowCheck checkRemove"></td>
+  <td>${data.name}</td>
+  <td class="data-Grade ${passColor}">${data.grade}%</td>
+  </tr>
+  `;
+}
+
+$(function(){
+  studentBody=$('#grade-table-body');
+});
+
+$('#student-input').on('submit',function(e){
+  e.preventDefault();
+  const formData = new FormData(e.target);
+  const data = {
+    name : formData.get('name'),
+    grade : formData.get('grade')
+  };
+  student.push(data);
+  gradeArray.push(parseInt(data.grade));
+//   console.log(gradeArray);
+  const newRow = createNewRow(data);
+  studentBody.append(newRow);
+  avgGrade();
+  $('#avg').html('Avg:' + `${avggrade}`);
+});
+
+$('#delete-btn').on('click',function(){
+  const checkedRows = $('#grade-table-body').find('.checkRemove:checked').closest('tr');
+  for(let i = gradeArray.length; i>=0;i--){
+    console.log(i);
+    if($(`#grade-table-body .checkRemove:eq(${i})`).is(':checked')){
+      gradeArray.splice(i,1);
+      student.splice(i,1);
+    //   console.log(gradeArray);
+    }
+  }
+  checkedRows.remove();
+  avgGrade();
+  $('#avg').html('Avg:' + `${avggrade}`);
+});
+
+function avgGrade(){
+  avggrade=0;
+  if(gradeArray.length != 0){ 
+    for(let i = 0; i<gradeArray.length;i++){
+        avggrade+=gradeArray[i];
+    }
+    avggrade/=gradeArray.length;
+    avggrade=avggrade.toFixed(2);
+  }
+}
+$('#select-all-checkbox').click(function(){
+  $('.rowCheck').prop('checked',$(this).prop('checked'));
+});
---
+++ test.js
@@ -0,0 +1,18 @@
+import { Selector } from 'testcafe'; // first import testcafe selectors
+
+fixture `Getting Started`// declare the fixture
+    .page `file:///C:/Users/chris/Bootcamp/gradebook/index.html`;  // specify the start page
+
+
+//then create a test and place your code there
+test('adding Content', async t => {
+    const passColor = (parseInt(data.grade)>=70) ? "green" : "red";
+    const gradeCell = Selector('dataGrade');
+    await t
+        .typeText('#form-name', 'LASKJLASDKJASLDJK')
+        .typeText('#form-grade','75')
+        .click('#submit-btn')
+
+        // Use the assertion to check if the actual header text is equal to the expected one
+        .expect(gradeCell).hasClass(passColor);
+});
